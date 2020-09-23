2d9696f4d6eb784afcdfb25a2eaac477bd23ced1
laptop <mnhaloka@gmail.com>
2020-09-23 17:54:14 +0200

    add key

--- lib/views/search/view.dart
+++ lib/views/search/view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:quot_app/const.dart';
 import 'package:quot_app/models/quotes.dart';
 import 'package:quot_app/views/full_quote/view.dart';
+import 'package:quot_app/widgets_ui/custom_circular_button.dart';
 
 Future<List<Quotes>> search(String search) async {
   await Future.delayed(Duration(seconds: 2));
@@ -16,20 +17,35 @@ class SearchView extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(20),
-        child: SearchBar<Quotes>(
-          onSearch: search,
-          hintText: "Type Quot",
-          placeHolder: Center(child: Image.asset("assets/img/search_page.png")),
-          onItemFound: (Quotes quot, int index) {
-            return ListTile(
-              onTap: () {
-                Navigator.pushNamed(context, FullQuoteView.id,
-                    arguments: quotesList[index]);
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: CustomCircularButton(
+                  onPress: () {
+                    Navigator.pop(context);
+                  },
+                  imgSrc: "assets/img/full-quot/left-arrow.png"),
+            ),
+            Expanded(
+              flex: 12,
+                child: SearchBar<Quotes>(
+              onSearch: search,
+              hintText: "Type Quot",
+              placeHolder:
+                  Center(child: Image.asset("assets/img/search_page.png")),
+              onItemFound: (Quotes quot, int index) {
+                return ListTile(
+                  onTap: () {
+                    Navigator.pushNamed(context, FullQuoteView.id,
+                        arguments: quotesList[index]);
+                  },
+                  title: Text("${quot.quotes}"),
+                  subtitle: Text("${quot.author}"),
+                );
               },
-              title: Text("${quot.quotes}"),
-              subtitle: Text("${quot.author}"),
-            );
-          },
+            )),
+          ],
         ),
       ),
     );
