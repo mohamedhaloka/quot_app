1bbdeb69212d4401b80f6c93ff8690d13f146061
laptop <mnhaloka@gmail.com>
2020-09-23 18:05:39 +0200

    add key

--- lib/widgets_ui/custom_circular_button.dart
+++ lib/widgets_ui/custom_circular_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class CustomCircularButton extends StatelessWidget {
   CustomCircularButton(
       {@required this.onPress, @required this.imgSrc, this.iconSize});
